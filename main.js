a643b075adfbc4f97907eff010a149b33e100458
Bhaskar Ghosh <bhaskar4647@gmail.com>
2022-09-06 20:19:14 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello, world!';
+
+/*This is a test case */
+// This is a test case that
+
+let iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+    alert('Yay, I love chocolate ice cream!');
+}else {
+    alert('Awww, but chocolate is my favorite ice cream!');
+}
+
+function multiply(a, b) {
+    let result = a * b;
+    return result;
+}
+
+multiply(4,7);
+multiply(20,20);
+multiply(0.5,3);
+
